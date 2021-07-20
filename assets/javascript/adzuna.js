aa5cacc84cd2887077b222ed37f4858fbf43c4a8
Mel Jack <melissa_jack@hotmail.co.uk>
2021-07-20 18:13:21 +0100

    Change http to https in adzuna API

--- assets/javascript/adzuna.js
+++ assets/javascript/adzuna.js
@@ -1,5 +1,5 @@
 function fetchJobList(job, city, filters) {
-    const requestURL = `http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=ab60a19a&app_key=4c8dd93a9e19f2fc2876eb639e148ef6&results_per_page=10&what=${job}&where=${city}&content-type=application/json`;
+    const requestURL = `https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=ab60a19a&app_key=4c8dd93a9e19f2fc2876eb639e148ef6&results_per_page=10&what=${job}&where=${city}&content-type=application/json`;
     fetch(requestURL)
         .then(function (response) {
             return response.json();
