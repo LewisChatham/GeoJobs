74ac12ef9d4d9a4ff103f29d4b9c9ddd06ba5cf1
LewisChatham <lewis.chatham@outlook.com>
2021-07-20 21:36:51 +0100

    Polished the UI and added flash message on error.

--- assets/javascript/adzuna.js
+++ assets/javascript/adzuna.js
@@ -6,14 +6,30 @@ function fetchJobList(job, city, filters) {
         })
         .then(function (data) {
             if (data.results.length == 0) {
-                searchResultsContainer.textContent = "Sorry: no results were found";
+                window.FlashMessage.error('No results could be found! Try to refine your search!', {
+                    progress: true,
+                    interactive: true,
+                    timeout: 8000,
+                    appear_delay: 200,
+                    container: '.flash-container',
+                    theme: 'default',
+                    classes: {
+                        container: 'flash-container',
+                        flash: 'flash-message',
+                        visible: 'is-visible',
+                        progress: 'flash-progress',
+                        progress_hidden: 'is-hidden'
+                    }
+                  });
                 return;
             }
 
             const searchParameters = {
                 job: job,
-                city: city
+                city: city,
+                amount: data.results.length
             }
+            addDescription(searchParameters);
             addPastSearch(searchParameters);
             displayPastSearches();
             parseJobListData(data);
